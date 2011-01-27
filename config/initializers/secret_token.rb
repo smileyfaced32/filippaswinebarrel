4b6d86fa50b7f483845b7b9d4088ae80a8da0995
smileyfaced32 <fiore.dennis@gmail.com>
2011-01-27 13:49:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Filippas::Application.config.secret_token = '49df672fb07dc1ec5b84ad6e9f8a4f49b4ad0f10abc616a426d2db164b04da0c97b86d1758599d01890d0c409c7fd1589381872b9bebdc99bd81f441a492fd32'
